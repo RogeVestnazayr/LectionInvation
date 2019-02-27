8cce0edd9aa8e0cadd64605ad99edbe822b2c551
RogeVestnazayr <31630996+RogeVestnazayr@users.noreply.github.com>
2019-02-27 07:50:48 +0200

    Добавьте файлы проекта.

---
+++ LectionInvites/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LectionInvites.Models;
+
+namespace LectionInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Добрый день" : "Добрый вечер";
+            return View();
+        }
+        [HttpGet]
+        public ViewResult InvationForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult InvationForm(Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                // TODO: Email response to the party organizer
+                return View("Thanks", student);
+            }
+            else
+            {
+                // there is a validation error
+                return View();
+            }
+          
+        }
+    }
+}
---
+++ LectionInvites/Models/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace LectionInvites.Models
+{
+    public class Student
+    {
+        [Required(ErrorMessage = "Пожалуйста введите ваше имя")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Пожалуйста введите вашу фамилию")]
+        public string LastName { get; set; }
+        [Required(ErrorMessage = "Пожалуйста введите ваш емейл")]
+        [RegularExpression(".+\\@.+\\..+",
+            ErrorMessage = "Введите коретный емейл")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Пожалуйста введите вашу групу")]
+        public string Group { get; set; }
+        [Required(ErrorMessage = "Пожалуйста определитесь с вашим выбором")]
+        public bool? WillAttend { get; set; }
+    }
+
+}
